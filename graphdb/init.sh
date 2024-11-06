a5b0a09519719cac7f8309d63468ca061d7eddb3
astein-iamovers <alvarostein1@gmail.com>
2024-11-06 15:58:04 -0800

    Update init.sh

--- graphdb/init.sh
+++ graphdb/init.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-NEONE_REPO1_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID")
+# Check if the repository already exists
+REPO_STATUS=$(curl -o /dev/null -s -w "%{http_code}" -X GET http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID")
 
-#NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
+if [ "$REPO_STATUS" -eq 404 ]; then
+    echo "Creating repository '$NEONE_REPO_ID'..."
+    curl -X POST --header 'Content-Type: multipart/form-data' \
+         -F "config=@/opt/neone/graphdb/neone-repository.ttl" \
+         http://graph-db:7200/rest/repositories
+    echo "Repository '$NEONE_REPO_ID' created."
 
-if [ "$NEONE_REPO1_HTTP_STATUS" -eq 404 ]; then
-    echo "Creating neone repository $NEONE_REPO_ID"
-    curl --fail -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository.ttl' 'http://graph-db:7200/rest/repositories'
-elif [ "$NEONE_REPO1_HTTP_STATUS" -eq 200 ]; then
-    echo "Repository already exists"
+    # Set the new repository as active
+    curl -X PUT --header 'Content-Type: text/plain' \
+         --data "$NEONE_REPO_ID" \
+         http://graph-db:7200/rest/activeRepository
+    echo "Repository '$NEONE_REPO_ID' set as active."
+elif [ "$REPO_STATUS" -eq 200 ]; then
+    echo "Repository '$NEONE_REPO_ID' already exists."
 else
-    echo "Error creating repository - Server returned $NEONE_REPO1_HTTP_STATUS for repo $NEONE_REPO_ID"
+    echo "Error checking repository status - Server returned $REPO_STATUS"
     exit 1
 fi
 
-# Set the repository as active after creating it
-curl -X PUT --header 'Content-Type: text/plain' \
-     --data "$NEONE_REPO_ID" \
-     http://graph-db:7200/rest/activeRepository
+#NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
 
 # Uncomment and edit the following lines to create a second repository if needed
 
