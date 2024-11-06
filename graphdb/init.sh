25a2244bd04700f22c880aa01b3d6bd81708cddb
astein-iamovers <alvarostein1@gmail.com>
2024-11-06 10:41:01 -0800

    Update init.sh

--- graphdb/init.sh
+++ graphdb/init.sh
@@ -2,7 +2,7 @@
 
 NEONE_REPO1_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID")
 
-NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
+#NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
 
 if [ "$NEONE_REPO1_HTTP_STATUS" -eq "404" ]; then
     echo "Creating neone repository $NEONE_REPO_ID"
@@ -14,12 +14,12 @@ else
   exit 1
 fi
 
-if [ "$NEONE_REPO2_HTTP_STATUS" -eq "404" ]; then
-    echo "Creating neone repository $NEONE_REPO_ID_2"
-    curl -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository-2.ttl' 'http://graph-db:7200/rest/repositories'    
-elif [ "$NEONE_REPO2_HTTP_STATUS" -eq "200" ]; then
-  echo "Repository already exixts"
-else
-  echo "Error creating repository - Server returned $NEONE_REPO2_HTTP_STATUS for repo $NEONE_REPO_ID_2"
-  exit 1
-fi
+#if [ "$NEONE_REPO2_HTTP_STATUS" -eq "404" ]; then
+#    echo "Creating neone repository $NEONE_REPO_ID_2"
+#    curl -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository-2.ttl' 'http://graph-db:7200/rest/repositories'    
+#elif [ "$NEONE_REPO2_HTTP_STATUS" -eq "200" ]; then
+#  echo "Repository already exixts"
+#else
+#  echo "Error creating repository - Server returned $NEONE_REPO2_HTTP_STATUS for repo $NEONE_REPO_ID_2"
+#  exit 1
+#fi
