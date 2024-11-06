b75a523d1a4b551d684d82eb15b1e8830c483c7f
astein-iamovers <alvarostein1@gmail.com>
2024-11-06 10:58:39 -0800

    Update init.sh

--- graphdb/init.sh
+++ graphdb/init.sh
@@ -4,22 +4,24 @@ NEONE_REPO1_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph
 
 #NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
 
-if [ "$NEONE_REPO1_HTTP_STATUS" -eq "404" ]; then
+if [ "$NEONE_REPO1_HTTP_STATUS" -eq 404 ]; then
     echo "Creating neone repository $NEONE_REPO_ID"
-    curl -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository.ttl' 'http://graph-db:7200/rest/repositories'    
-elif [ "$NEONE_REPO1_HTTP_STATUS" -eq "200" ]; then
-  echo "Repository already exixts"
+    curl --fail -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository.ttl' 'http://graph-db:7200/rest/repositories'
+elif [ "$NEONE_REPO1_HTTP_STATUS" -eq 200 ]; then
+    echo "Repository already exists"
 else
-  echo "Error creating repository - Server returned $NEONE_REPO1_HTTP_STATUS for repo $NEONE_REPO_ID"
-  exit 1
+    echo "Error creating repository - Server returned $NEONE_REPO1_HTTP_STATUS for repo $NEONE_REPO_ID"
+    exit 1
 fi
 
-#if [ "$NEONE_REPO2_HTTP_STATUS" -eq "404" ]; then
+# Uncomment and edit the following lines to create a second repository if needed
+
+#if [ "$NEONE_REPO2_HTTP_STATUS" -eq 404 ]; then
 #    echo "Creating neone repository $NEONE_REPO_ID_2"
-#    curl -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository-2.ttl' 'http://graph-db:7200/rest/repositories'    
-#elif [ "$NEONE_REPO2_HTTP_STATUS" -eq "200" ]; then
-#  echo "Repository already exixts"
+#    curl --fail -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository-2.ttl' 'http://graph-db:7200/rest/repositories'
+#elif [ "$NEONE_REPO2_HTTP_STATUS" -eq 200 ]; then
+#    echo "Repository already exists"
 #else
-#  echo "Error creating repository - Server returned $NEONE_REPO2_HTTP_STATUS for repo $NEONE_REPO_ID_2"
-#  exit 1
+#    echo "Error creating repository - Server returned $NEONE_REPO2_HTTP_STATUS for repo $NEONE_REPO_ID_2"
+#    exit 1
 #fi
