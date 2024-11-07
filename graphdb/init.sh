1338631694ddc3fb3357b66d449fec5d16d23513
astein-iamovers <alvarostein1@gmail.com>
2024-11-06 16:57:36 -0800

    Update init.sh

--- graphdb/init.sh
+++ graphdb/init.sh
@@ -21,17 +21,3 @@ else
     echo "Error checking repository status - Server returned $REPO_STATUS"
     exit 1
 fi
-
-#NEONE_REPO2_HTTP_STATUS=$(curl -o /dev/null -s -w '%{http_code}' -i http://graph-db:7200/rest/repositories/"$NEONE_REPO_ID_2")
-
-# Uncomment and edit the following lines to create a second repository if needed
-
-#if [ "$NEONE_REPO2_HTTP_STATUS" -eq 404 ]; then
-#    echo "Creating neone repository $NEONE_REPO_ID_2"
-#    curl --fail -X POST --header 'Content-Type:multipart/form-data' -F 'config=@/opt/neone/graphdb/neone-repository-2.ttl' 'http://graph-db:7200/rest/repositories'
-#elif [ "$NEONE_REPO2_HTTP_STATUS" -eq 200 ]; then
-#    echo "Repository already exists"
-#else
-#    echo "Error creating repository - Server returned $NEONE_REPO2_HTTP_STATUS for repo $NEONE_REPO_ID_2"
-#    exit 1
-#fi
